47b0a4330a0d6db67e3e8379112dbbf5bdada873
Dmitry V. Luciv <dluciv@dluciv.name>
2022-05-22 20:50:01 +0300

    Prof

--- examples/05.Pointers/pointer1.c
+++ examples/05.Pointers/pointer1.c
@@ -3,12 +3,16 @@
 int main()
 {
   int a[1000], v, *p = a;
-  printf("%u\t%u\n", &v, a);
-  printf("%u\t%u\t%u\n", &a[0], a + 1, &a[5000]);
-  printf("%u\t%u\n", p++, p);
+  printf("%p\t%p\n", &v, a);
+  printf("%p\t%p\t%p\n", &a[0], a + 1, &a[5000]);
+  printf("%p\t%p\n", p++, p);
 
-  int *p1 = a + 800;
+  double *p1 = (void *)(a + 800);
   printf("%d\t%d\t%d\t%d\n", *p1, *(p1 + 1), p1[0], p1[1]);
+
+  void *vp = a;
+  printf("%d, %p, %p\n", sizeof(*vp), vp, vp + 1);
+  printf("%d\n", *(int *)(vp + 1));
 
   return 0;
 }
---
+++ examples/09.2.Tools_Profile/README.md
@@ -0,0 +1,4 @@
+Inspired by: https://www.thegeekstuff.com/2012/08/gprof-tutorial/
+
+1. `cc -Wall -pg test_gprof.c -o test_gprof`
+2. `gprof test_gprof gmon.out > analysis.txt`
